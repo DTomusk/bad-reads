828ffd90f22429f69fc40de413a2e47130e49e9e
Dave <david.tomusk@gmail.com>
2025-06-04 08:44:52 +0100

    Update image to contain thumbnail

--- ui/src/components/BookCard.tsx
+++ ui/src/components/BookCard.tsx
@@ -53,7 +53,12 @@ export default function BookCard({
         style={{ width: "20rem", height: "20rem" }}
       >
         <Card.Section>
-          <Image src={picture_url} height={160} alt={`${title} image`} />
+          <Image 
+            src={picture_url} 
+            height={160} 
+            alt={`${title} image`} 
+            fit="contain"
+          />
         </Card.Section>
 
         <Group justify="space-between" mt="md" mb="xs">
