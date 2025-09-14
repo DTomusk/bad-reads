e35c41469bdaaca3c70eb20ad0234e53924adb05
Dave <david.tomusk@gmail.com>
2025-09-14 17:55:06 +0100

    Add global stats table

--- api/src/books/application/models.py
+++ api/src/books/application/models.py
@@ -78,3 +78,16 @@ class AuthorModel(Base):
 
     def __repr__(self):
         return f"<AuthorModel(id={self.id}, name={self.name})>"
+    
+class GlobalRatingStatsModel(Base):
+    __tablename__ = "global_rating_stats"
+
+    id = Column(Integer, primary_key=True, default=1)
+    
+    num_love_ratings = Column(Integer, nullable=False, default=0)
+    sum_love_ratings = Column(Float, nullable=False, default=0.0)
+    mean_love_rating = Column(Float, nullable=False, default=0.0)
+
+    num_shit_ratings = Column(Integer, nullable=False, default=0)
+    sum_shit_ratings = Column(Float, nullable=False, default=0.0)
+    mean_shit_rating = Column(Float, nullable=False, default=0.0)
