228ccc8bd821b5bfb044111621698794f3058c48
Dave <david.tomusk@gmail.com>
2025-09-22 15:52:22 +0100

    Remove references to ratings and api in review repo

--- api/src/books/application/repositories/review_repository.py
+++ api/src/books/application/repositories/review_repository.py
@@ -4,10 +4,8 @@ from datetime import datetime, timezone
 
 from sqlalchemy.orm import Session
 
-from src.books.domain.models import Rating, RatingScore, Review
-from src.books.application.models import ReviewModel, RatingModel
-# TODO: the repo shouldn't know about the api schemas 
-from src.books.api.schemas.review_response import ReviewResponse
+from src.books.domain.models import Review
+from src.books.application.models import ReviewModel
 
 class AbstractReviewRepo(ABC):
     @abstractmethod
@@ -20,7 +18,7 @@ class AbstractReviewRepo(ABC):
         pass
 
     @abstractmethod
-    def get_reviews_by_book_id(self, book_id: UUID, sort_by: str = "date_created", sort_order: str = "desc", limit: int = 10) -> list[ReviewResponse]:
+    def get_reviews_by_book_id(self, book_id: UUID, sort_by: str = "date_created", sort_order: str = "desc", limit: int = 10) -> list[Review]:
         """
         Get all reviews for a book.
         :param book_id: The ID of the book to get reviews for.
@@ -32,7 +30,7 @@ class AbstractReviewRepo(ABC):
         pass
 
     @abstractmethod
-    def get_reviews_by_user_id(self, user_id: UUID) -> list[ReviewResponse]:
+    def get_reviews_by_user_id(self, user_id: UUID) -> list[Review]:
         """
         Get all reviews for a user.
         :param user_id: The ID of the user to get reviews for.
@@ -57,7 +55,7 @@ class AbstractReviewRepo(ABC):
         pass
 
     @abstractmethod
-    def get_review_by_user_and_book(self, user_id: UUID, book_id: UUID) -> ReviewResponse:
+    def get_review_by_user_and_book(self, user_id: UUID, book_id: UUID) -> Review:
         """
         Get a review by user and book.
         :param user_id: The ID of the user.
@@ -96,30 +94,8 @@ class ReviewRepo(AbstractReviewRepo):
         self.session.add(review_model)
         self.session.commit()
 
-    def _create_review_response(self, review_model: ReviewModel) -> ReviewResponse:
-        rating_model = self.session.query(RatingModel).filter(RatingModel.id == review_model.rating_id).first()
-        if not rating_model:
-            return None
-            
-        review = Review(
-            id=review_model.id,
-            book_id=review_model.book_id,
-            user_id=review_model.user_id,
-            text=review_model.text,
-            rating_id=review_model.rating_id,
-            date_created=review_model.date_created
-        )
-        rating = Rating(
-            book_id=review_model.book_id,
-            user_id=review_model.user_id,
-            id=rating_model.id,
-            love_score=RatingScore(rating_model.love_score),
-            shit_score=RatingScore(rating_model.shit_score)
-        )
-        return ReviewResponse.from_domain(review, rating)
-
-    def get_reviews_by_book_id(self, book_id: UUID, sort_by: str = "date_created", sort_order: str = "desc", limit: int = 10) -> list[ReviewResponse]:
-        result = (self.session.query(ReviewModel)
+    def get_reviews_by_book_id(self, book_id: UUID, sort_by: str = "date_created", sort_order: str = "desc", limit: int = 10) -> list[Review]:
+        result: list[ReviewModel] = (self.session.query(ReviewModel)
             .filter(ReviewModel.book_id == book_id, ReviewModel.date_deleted.is_(None))
             .order_by(getattr(ReviewModel, sort_by).asc() if sort_order == "asc" else getattr(ReviewModel, sort_by).desc())
             .limit(limit)
@@ -127,27 +103,27 @@ class ReviewRepo(AbstractReviewRepo):
         if not result:
             return []
         
-        review_responses = []
-        for review_model in result:
-            review_response = self._create_review_response(review_model)
-            if review_response:
-                review_responses.append(review_response)
-        
-        return review_responses
+        return [Review(
+            id=review.id, 
+            book_id=review.book_id,
+            user_id=review.user_id,
+            text=review.text,
+            rating_id=review.rating_id,
+            date_created=review.date_created) for review in result]
 
-    def get_reviews_by_user_id(self, user_id: UUID) -> list[ReviewResponse]:
-        result = self.session.query(ReviewModel).filter(ReviewModel.user_id == user_id, ReviewModel.date_deleted.is_(None)).all()
+    def get_reviews_by_user_id(self, user_id: UUID) -> list[Review]:
+        result: list[ReviewModel] = self.session.query(ReviewModel).filter(ReviewModel.user_id == user_id, ReviewModel.date_deleted.is_(None)).all()
         if not result:
             return []
         
-        review_responses = []
-        for review_model in result:
-            review_response = self._create_review_response(review_model)
-            if review_response:
-                review_responses.append(review_response)
+        return [Review(
+            id=review.id, 
+            book_id=review.book_id,
+            user_id=review.user_id,
+            text=review.text,
+            rating_id=review.rating_id,
+            date_created=review.date_created) for review in result]
         
-        return review_responses
-
     def get_review_by_id(self, review_id: UUID) -> Review:
         result = self.session.query(ReviewModel).filter(ReviewModel.id == review_id).first()
         if not result:
@@ -174,15 +150,21 @@ class ReviewRepo(AbstractReviewRepo):
             date_created=result.date_created
         )
     
-    
-    def get_review_by_user_and_book(self, user_id: UUID, book_id: UUID) -> ReviewResponse:
+    def get_review_by_user_and_book(self, user_id: UUID, book_id: UUID) -> Review:
         result = self.session.query(ReviewModel).filter(
             ReviewModel.user_id == user_id, 
             ReviewModel.book_id == book_id
         ).first()
         if not result:
             return None
-        return self._create_review_response(result)
+        return  Review(
+            id=result.id,
+            book_id=result.book_id,
+            user_id=result.user_id,
+            text=result.text,
+            rating_id=result.rating_id,
+            date_created=result.date_created
+        )
     
     # Updating a review also undeletes it
     def update_review(self, review_id: UUID, text: str):
